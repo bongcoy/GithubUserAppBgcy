be39e4b271e9e4d605034809305dc987c6db93a7
Cal <mpascaldewantara@gmail.com>
2021-08-20 12:02:28 +0700

    Fix RecyclerView related, start modify Detail Activity

--- app/src/main/java/com/example/githubuserappbgcy/detail/DetailActivity.kt
+++ app/src/main/java/com/example/githubuserappbgcy/detail/DetailActivity.kt
@@ -2,11 +2,49 @@ package com.example.githubuserappbgcy.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.githubuserappbgcy.R
+import com.example.githubuserappbgcy.databinding.ActivityDetailBinding
+import com.example.githubuserappbgcy.list.User
 
 class DetailActivity : AppCompatActivity() {
+    private val activityTitle : String = "Detail User"
+    private lateinit var binding: ActivityDetailBinding
+
+    companion object{
+        const val EXTRA_USER = "extra_user"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setActionBarTitle(activityTitle)
+
+        val user = intent.getParcelableExtra<User>(EXTRA_USER) as User
+
+        Glide.with(this)
+            .load(user.avatar)
+            .apply(RequestOptions.overrideOf(R.dimen.avatar_big))
+            .into(binding.ivUserProfile)
+        binding.apply {
+            tvUsername.text = user.username
+            tvName.text = user.name
+            tvFollowerNumber.text = user.followers.toString()
+            tvFollowingNumber.text = user.followings.toString()
+            tvRepositoryNumber.text = user.repository.toString()
+            tvCompany.text = user.company
+            tvLocation.text = user.location
+        }
+    }
+
+    private fun setActionBarTitle(title : String) {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = title
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
--- app/src/main/java/com/example/githubuserappbgcy/list/ListUserAdapter.kt
+++ app/src/main/java/com/example/githubuserappbgcy/list/ListUserAdapter.kt
@@ -11,6 +11,15 @@ import com.example.githubuserappbgcy.databinding.ItemUserBinding
 
 class ListUserAdapter (private val listUser: ArrayList<User>) : RecyclerView.Adapter<ListUserAdapter.ListViewHolder>() {
     private lateinit var binding: ItemUserBinding
+    private lateinit var onItemClickCallback : OnItemClickCallback
+
+    interface OnItemClickCallback {
+        fun onItemClicked(item: User)
+    }
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback){
+        this.onItemClickCallback = onItemClickCallback
+    }
 
     inner class ListViewHolder(binding: ItemUserBinding): RecyclerView.ViewHolder(binding.root)
 
@@ -21,11 +30,18 @@ class ListUserAdapter (private val listUser: ArrayList<User>) : RecyclerView.Ada
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val user = listUser[position]
+
+        binding.tvUsername.text = user.username
+        binding.tvRepositoryNumber.text = user.repository.toString()
+        binding.tvFollowerNumber.text = user.followers.toString()
         Glide.with(holder.itemView.context)
             .load(user.avatar)
             .apply(RequestOptions.overrideOf(R.dimen.avatar_small,R.dimen.avatar_small))
-            .into(holder.binding.iv_avatar)
-        binding.tvFollowerNumber.text =
+            .into(binding.ivAvatar)
+
+        holder.itemView.setOnClickListener{
+            onItemClickCallback.onItemClicked(listUser[holder.adapterPosition])
+        }
     }
 
     override fun getItemCount(): Int = listUser.size
